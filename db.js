83d19d9cb9603e7591779a800ce56d794b7cee49
Berjooj <moreira.bernardo@yahoo.com.br>
2018-11-01 21:30:22 -0300

    db upgradec 1

--- db.js
+++ db.js
@@ -2,17 +2,18 @@ var mongoCliente = require("mongodb").MongoClient;
 mongoCliente.connect("mongodb://admin2:YDEs274Yb3cfELJ@ds235807.mlab.com:35807/heroku_tbhttlkq",
     function (err, conn) {
         if (err) return console.log(err);
-        
-        findDocuments(client.db('myBD'), function() {
-            db.close();
-        });
+        var db = client.db('heroku_tbhttlkq"');
 
-        global.db = conn;
+        db.collection('user').findOne({}, function (findErr, result) {
+          if (findErr) throw findErr;
+          console.log(result.name);
+          client.close();
+        });
     });
 
 
     function insertNewUser (firstName, lastName, callbakc) {
-        global.db.collection("myBD").insert({firstName, lastName}, 
+        global.db.collection("user").insert({firstName, lastName}, 
             function (err, result) {
                 if (err) return console.log(err);
                 callbakc();
