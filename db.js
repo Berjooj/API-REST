d18f96c8a8a27110932dbf586d2c4c0d0c7d3072
Berjooj <moreira.bernardo@yahoo.com.br>
2018-11-01 21:37:48 -0300

    test bd

--- db.js
+++ db.js
@@ -2,18 +2,19 @@ var mongoCliente = require("mongodb").MongoClient;
 mongoCliente.connect("mongodb://admin2:YDEs274Yb3cfELJ@ds235807.mlab.com:35807/heroku_tbhttlkq",
     function (err, conn) {
         if (err) return console.log(err);
-            
+        else console.log("connected");
         global.db = conn;
     });
 
 
-    function insertNewUser (firstName, lastName, callbakc) {
-        global.db.collection("user").insert({firstName, lastName}, 
-            function (err, result) {
-                if (err) return console.log(err);
-                callbakc();
-            });
-    }
+    // function insertNewUser (firstName, lastName, callbakc) {
+    //     global.db.collection("user").insert({firstName, lastName}, 
+    //         function (err, result) {
+    //             if (err) return console.log(err);
+    //             callbakc();
+    //         });
+    // }
 
 
-    module.exports = {insertNewUser};
+    // module.exports = {insertNewUser};
+    module.exports = {};
