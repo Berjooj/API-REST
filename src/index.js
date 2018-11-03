be39d77180425be39f5774b4cf4667b33958462e
Berjooj <moreira.bernardo@yahoo.com.br>
2018-11-03 04:07:15 -0300

    v0.2.4

--- src/index.js
+++ src/index.js
@@ -8,21 +8,19 @@ let movieRoute = require("./routes/movie.route");
 
 let session = require("express-session");
 
-// Start server
-const PORT = process.env.PORT | 3000;
-
 //Middleware
-app.use(session({
-    secret: 'ahiu33uh2iSIUAH22', // session secret
-    resave: false,
-    saveUninitialized: true
-    }));
+// app.use(session({
+//     secret: 'ahiu33uh2iSIUAH22', // session secret
+//     resave: false,
+//     saveUninitialized: true
+//     }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express.static("public"));
-app.use(userRoute);
-app.use(movieRoute);
+// app.use(userRoute);
+// app.use(movieRoute);
 
-
+// Start server
+const PORT = process.env.PORT | 3000;
 app.listen(PORT, () => console.info(`Server has started on ${PORT}`));
